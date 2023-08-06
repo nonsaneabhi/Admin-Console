415551a2eec9fcaeb3ef205e0cba093e2c82e2ef
nonsaneabhi <aa14142001@gmail.com>
2023-08-06 20:34:26 +0530

    added routing configs to UI for billboards

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -7,6 +7,7 @@ import { useParams, useRouter } from "next/navigation";
 import { Plus } from "lucide-react";
 import { BillboardColumn, columns } from "./columns";
 import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
 
 interface BillboardClientProps {
     data : BillboardColumn[]
@@ -28,6 +29,9 @@ export const BillboardClient : React.FC<BillboardClientProps> = ({data}) => {
         </div>
         <Separator />
         <DataTable columns={columns} data={data} searchKey="label" />
+        <Heading title="API" description="API Calls for Billboards" />
+        <Separator />
+        <ApiList entityName="billboards" entityIdName="billboardId" />
         </>
     )
 }
